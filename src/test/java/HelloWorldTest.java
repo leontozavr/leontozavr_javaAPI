e7a7c6b9b40fd44b6003b6d9b364991fa0ee8dc4
leontozavr <theflaffily@gmail.com>
2025-05-16 11:34:31 +0300

    Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,25 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
+
     @Test
-    public void testHelloWorld() {
+    public void Ex4() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void Ex5() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework");
+        String responseBody = response.getBody().asString();
+        JsonPath json = new JsonPath(responseBody);
+        String secondMessage = json.get("messages[1].message");
+        System.out.println(secondMessage);
     }
 }
